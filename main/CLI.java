a64e0d28135630b51bd7613b439b58dda4bfb423
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-11-01 22:34:57 +0200

    Add functions for some commands

--- main/CLI.java
+++ main/CLI.java
@@ -132,6 +132,130 @@ public class CLI {
     // Mohamed 
 
 
+    public void mv(String str){
+        List<String> files = new ArrayList<>(Arrays.asList(str.split(" ")));
+
+        Path target = currDir.resolve(files.get(files.size() - 1)); // last file or dir
+        List<Path> source = new ArrayList<>(); // list if multiple sorces
+        for (int i = 0 ; i < files.size() - 1 ; i++)
+            source.add(currDir.resolve(files.get(i)));
+
+        try{
+            if (source.size() == 1){
+
+                Path src = source.get(0);
+
+                if (!Files.exists(src)) {
+                    throw new FileNotFoundException(src.getFileName() + " -> Not Found");
+                }
+
+                if (Files.isDirectory(src)){
+                    if (Files.notExists(target)){
+                        Files.move(src , src.resolveSibling(target.toString()));
+                    }
+                    else if (Files.isDirectory(target)){
+                        Files.move(src, target.resolve(src.getFileName()) , StandardCopyOption.REPLACE_EXISTING);
+                    }
+                    else {
+                        throw new FileNotFoundException("Cannot overwrite non-directory " + src.toFile().getName() + " with directory " + target.toFile().getName());
+                    }
+                }
+                else if (!Files.isDirectory(src)){
+                    if (Files.notExists(target)){
+                        Files.move(src , src.resolveSibling(target.toString())); // rename
+                    }
+                    else if (Files.isDirectory(target)){
+                        Files.move(src , src.resolveSibling(target.toString()));
+                    }
+                    else {
+                        Files.move(src, target, StandardCopyOption.REPLACE_EXISTING);
+                    }
+                }
+            }
+            else { //  multiple sources
+                if (Files.notExists(target) || !Files.isDirectory(target)) {
+                    throw new IOException("Target must be an existing directory for multiple sources");
+                }
+
+                for (Path file : source){
+                    if (Files.notExists(file)) {
+                        throw new FileNotFoundException("Source file/directory does not exist: " + file.toFile().getName());
+                    }
+                    else
+                        Files.move(file, target.resolve(file.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+                }
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // -----------------------------------------------
+    // -----------------------------------------------
+
+    private String execute(String[] str){
+
+        String command = str[0].trim();
+        String content  = "" ;
+
+        if (command.equals("ls")) {
+            ArrayList<String> files = ls();
+            content = String.join("\n" , files);
+        }
+        else if (command.equals("echo")){
+            StringBuilder s = new StringBuilder();
+            for (int i = 1 ; i < str.length ; i++){
+                if (str[i].equals(">"))
+                    break;
+                s.append(str[i]).append(" ");
+            }
+            content = s.toString();
+        }
+        else if (command.equals("cat"))
+            content = cat(str[1].split(" "));
+
+        return content;
+    }
+
+    public void redirect(String content , String file , boolean append){  // for > and >> 
+        try{
+            FileWriter w = new FileWriter(file , append);
+            w.write(content);
+            w.write("\n");
+            w.close();
+        }
+        catch (IOException e){
+            System.out.println("Error writing ti file : " + e.getMessage());
+        }
+    }
+
+    // -----------------------------------------------
+    // -----------------------------------------------
+
+    public ArrayList<String> ls(){
+        ArrayList<String> files = new ArrayList<>();
+        if (Files.isDirectory(currDir)){
+            try{
+                DirectoryStream<Path> streamDir = Files.newDirectoryStream(currDir);
+                for (Path file : streamDir)
+                    files.add(file.getFileName().toString());
+
+                Collections.sort(files);
+            }
+            catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        }
+        else
+            System.out.println("Current path not directory");
+
+        return files;
+    }
+
 
 
 }
