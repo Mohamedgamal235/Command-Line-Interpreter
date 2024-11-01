476ac70c73731ba3d1d168e847302dd40856e06a
Mohamed Gamal Ali <gemmymo6@gmail.com>
2024-11-01 22:22:02 +0200

    Add test folder for test CLI class using JUnit

---
+++ test/CLITest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static java.nio.file.Files.readString;
+import static org.junit.jupiter.api.Assertions.*;
+import org.AssignemntOS.CLI;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class CLITest {
+
+    
+
+}
