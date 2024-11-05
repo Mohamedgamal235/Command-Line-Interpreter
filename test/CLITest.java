790a69b0c71f76da2f70985cf3439eb442e2e455
Marwa Haron <115185549+marwaharon27@users.noreply.github.com>
2024-11-05 19:28:19 +0200

    Update CLITest.java

--- test/CLITest.java
+++ test/CLITest.java
@@ -8,7 +8,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
+import java.util.List;
+import java.util.Arrays;
 public class CLITest {
 
     private CLI cli;
